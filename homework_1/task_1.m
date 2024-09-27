8a92b2cb12a14370a0eb7d1eb671f9d6397ad6ca
Nasir-A-2006 <ashley59@students.rowan.edu>
2024-09-27 15:53:49 +0000

    finished task 1

---
+++ homework_1/task_1.m
@@ -0,0 +1,21 @@
+% Author Name: Nasir Ashley
+% Email: [Your Email]
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Assignment 2
+% Task: [Task Name]
+% Date: [Submission Date]
+
+celsius = input('Enter the temp in celsius:');
+
+fahrenheit =(celsius * 9/5) + 32;
+
+fprintf('The temp in fahrenheit is %.2f\n', fahrenheit);
+
+if fahrenheit > 100
+    fprintf('IT''S A HOT DAY!!!!!')
+end
+
+
+% git add homework_1/task_1.m
+%git commit -m 'finished task 1'    %%%%%% 'the message'
+%git push origin main               %%%%%%%%% the name of the branch in red 
